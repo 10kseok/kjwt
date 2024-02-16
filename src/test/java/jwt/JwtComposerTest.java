20ac48a17aa36cf46dbeb49378fda55587897767
koesnam <76582376+10kseok@users.noreply.github.com>
2024-02-16 10:38:35 +0900

    test: add JwtComposerTest
    test for default creating JWT

--- src/test/java/jwt/JwtComposerTest.java
+++ src/test/java/jwt/JwtComposerTest.java
@@ -1,5 +1,12 @@
 package jwt;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
 /**
  * {설명을 작성해주세요}
  *
@@ -8,10 +15,11 @@ package jwt;
  */
 public class JwtComposerTest {
 
-//    public void testCreateJwt() throws NoSuchAlgorithmException, InvalidKeyException {
-//        Map<String, Object> expectHeader = Map.of("alg", "HS256", "typ", "JWT");
-//        Map<String, Object> expectPayload = Map.of("jti", 1, "iss", LocalDate.now().toString());
-//        String actual = JwtComposer.createToken(expectHeader, expectPayload, "jwtSecretForHMACWithSHA-256".getBytes(StandardCharsets.UTF_8));
-//        System.out.println(actual);
-//    }
+    @Test
+    public void testCreateJwt() throws NoSuchAlgorithmException, InvalidKeyException {
+        Map<String, Object> expectHeader = Map.of("alg", "HS256", "typ", "JWT");
+        Map<String, Object> expectPayload = Map.of("jti", 1, "iss", LocalDate.now().toString());
+        Jwt actual = JwtComposer.compose(expectHeader, expectPayload, "jwtSecretForHMACWithSHA-256".getBytes(StandardCharsets.UTF_8));
+        System.out.println(actual);
+    }
 }
